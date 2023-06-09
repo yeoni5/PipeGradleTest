002b96fb77abbe477a2c37bb892ddff00ff08a19
yeoni5 <yeoni5@naver.com>
2023-06-09 18:28:22 +0900

    15

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
         <header>
           <img src={ img1 } style={{ width: '200px' }} alt='' />  
                
-          <h1>Test-108</h1>          
+          <h1>Test-110</h1>          
           <ul style={{ listStyle: 'none' }}>
             {
               message.map((text, index) => <li key={ index }>
