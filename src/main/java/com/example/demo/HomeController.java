66e69cac7c602043183634c240b08a1c380768a2
yeoni5 <yeoni5@naver.com>
2023-06-09 14:53:10 +0900

    7

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,9 +6,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HomeController {
 	
